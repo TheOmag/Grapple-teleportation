0135beda59b1b8a06bc09f645c29f3bf36971484
Crafterbot <71361713+TheOmag@users.noreply.github.com>
2021-11-04 23:23:11 -0400

    Add files via upload

---
+++ assets/Patcher.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using BepInEx;
+using System.Reflection;
+
+namespace patcher
+{
+
+    [BepInPlugin("org.Crafterbot.grapple.TeleportationGrappleMod", "Teleportation Grapple", "5.4.17.0")]
+    public class MyPatcher : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var harmony = new Harmony("com.Crafterbot.grapple.TeleportationGrappleMod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ assets/teleport.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace teleport
+{
+    [HarmonyPatch(typeof(Wildflare.Player.CameraController))]
+    [HarmonyPatch("Update", MethodType.Normal)]
+
+    public class code
+    {
+        private static bool hitOneOff = false;
+        private static void Postfix(Wildflare.Player.CameraController __instance)
+        {
+            if (Input.GetKey(KeyCode.T) && !hitOneOff)
+            {
+                Ray mainRay = new Ray(__instance.playerCam.transform.position + new Vector3(0f, 0f, 0f), __instance.playerCam.transform.forward);
+                Physics.Raycast(mainRay, 10000);
+                RaycastHit hit;                                                                                                                                                               
+                if (Physics.Raycast(mainRay, out hit))
+                {
+                    Wildflare.Player.PlayerMovement.Instance.transform.position = hit.point + new Vector3(0f, 4.4f, 0f);
+                }
+                hitOneOff = true;
+            }
+            if (!Input.GetKey(KeyCode.T))
+            {
+                hitOneOff = false;
+            }
+        }
+    }
+}
